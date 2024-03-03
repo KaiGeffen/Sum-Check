4920c32fc53010cf958c97e247ac0b722af24d23
KaiGeffen <geffenkai@gmail.com>
2024-03-03 08:41:23 -0500

    Rearranged and gave better naming to some formulas/method

--- lib/formula.ml
+++ lib/formula.ml
@@ -8,7 +8,15 @@ type pform =
 | And of pform * pform
 | Or of pform * pform
 
-let rec show_pform formula =
+(* Arithmetic formulas *)
+type aform =
+| Const of int
+| Variable of string
+| Add of aform * aform
+| Sub of aform * aform
+| Mul of aform * aform
+
+let rec show_pform (formula : pform) =
   match formula with
   | Const false -> "False"
   | Const true -> "True"
@@ -18,15 +26,7 @@ let rec show_pform formula =
   | And (f1, f2) -> "(" ^ show_pform f1 ^ " ^ " ^ show_pform f2 ^ ")"
   | Or (f1, f2) -> "(" ^ show_pform f1 ^ " ∨ " ^ show_pform f2 ^ ")"
 
-(* Arithmetic formulas *)
-type aform =
-| Const of int
-| Variable of string
-| Add of aform * aform
-| Sub of aform * aform
-| Mul of aform * aform
-
-let rec show_aform formula =
+let rec show_aform (formula : aform) =
   match formula with
   | Const c -> string_of_int c
   (* Reserve numbers *)
@@ -34,20 +34,20 @@ let rec show_aform formula =
   | Add (f1, f2) -> "(" ^ show_aform f1 ^ " + " ^ show_aform f2 ^ ")"
   | Sub (f1, f2) -> "(" ^ show_aform f1 ^ " - " ^ show_aform f2 ^ ")"
   | Mul (f1, f2) -> show_aform f1 ^ " * " ^ show_aform f2
+
 let rec arithmetize (formula : pform) : aform =
-    match formula with
-    | Const c -> if c then Const 1 else Const 0
-    | Variable s -> Variable s
-    (* NOT A -> (1 - A) *)
-    | Not f -> Sub (Const 1, arithmetize f)
-    (* A AND B -> A * B *)
-    | And (f1, f2) -> Mul (arithmetize f1, arithmetize f2)
-    (* A OR B -> (A + B) - (A * B) *)
-    | Or (f1, f2) -> Sub (
-      Add (arithmetize f1, arithmetize f2),
-      Mul (arithmetize f1, arithmetize f2)
-      );;
-  
+  match formula with
+  | Const c -> if c then Const 1 else Const 0
+  | Variable s -> Variable s
+  (* NOT A -> (1 - A) *)
+  | Not f -> Sub (Const 1, arithmetize f)
+  (* A AND B -> A * B *)
+  | And (f1, f2) -> Mul (arithmetize f1, arithmetize f2)
+  (* A OR B -> (A + B) - (A * B) *)
+  | Or (f1, f2) -> Sub (
+    Add (arithmetize f1, arithmetize f2),
+    Mul (arithmetize f1, arithmetize f2)
+    );;
 
 let rec eval formula : int =
   match formula with
@@ -58,15 +58,15 @@ let rec eval formula : int =
   | Sub (f1, f2) -> ((eval f1) - (eval f2)) % field_size
   | Mul (f1, f2) -> ((eval f1) * (eval f2)) % field_size
 
-  let rec get_first_free_variable formula : string option =
-    match formula with
-    | Const _ -> None
-    | Variable v -> Some v
-    | Add (f1, f2)
-    | Sub (f1, f2)
-    | Mul (f1, f2) -> match get_first_free_variable f1 with
-      | Some v -> Some v
-      | None -> get_first_free_variable f2
+let rec get_first_free_variable formula : string option =
+  match formula with
+  | Const _ -> None
+  | Variable v -> Some v
+  | Add (f1, f2)
+  | Sub (f1, f2)
+  | Mul (f1, f2) -> match get_first_free_variable f1 with
+    | Some v -> Some v
+    | None -> get_first_free_variable f2
   
 (* Constrain the given variable to value in the aformula *)
 let rec constrain (formula : aform) (variable : string) (value : int) =
@@ -89,7 +89,6 @@ let constrain_first (formula : aform) (value : int) : aform  =
 
 let eval_monomial formula value : int =
   eval (constrain_first formula value)
-
 
 let rec get_sharp_sat (formula : aform) : int =
   match get_first_free_variable formula with
