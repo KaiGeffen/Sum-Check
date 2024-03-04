1fc56906dfae9da1a4f91e3256e05ba8a10cca9c
KaiGeffen <geffenkai@gmail.com>
2024-03-04 18:34:02 -0500

    Made main function arguments named, adjusted and documented their names

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,9 @@
-(* main.ml *)
-
 open Sum_Check
 open Formula
 open Prover
 open Verifier
 
+(* Step 0 - Get and print the formula *)
 (* (X1 ∧ ¬X2) ∨ (X3 ∨ (X4 ∧ ¬X5)) *)
 (* Has 23 satisfying assignments *)
 let example_pform =
@@ -14,35 +13,28 @@ let example_pform =
       Variable "X4", Not(Variable "X5"))
     )
   );;
-
-(* Step 1 *)
 Printf.printf "Propositional formula:\n%s\n\n" (show_pform example_pform);;
 let g0 : aform = arithmetize example_pform;;
 Printf.printf "Arithmetic representation:\n%s\n\n" (show_aform g0);;
 
-(* Step 2 *)
-(* 
-  Given a formula with at least one free variable
-  form the partial equation that leaves the first variable free
-  and sums over the superset of all other variables   
+(* Perform the full sumcheck protocol
+  g0: The starting (arithmetic) function
+  g: The current function (constrained by the random numbers)
+  round: The round number
+  randoms: The current list of random numbers [r1,r2,...,ri]
 *)
-(* let g1_part : aform = Prover.get_partial_sum g0;;
-Printf.printf "partial_sum = %s\n" (show_aform g1_part);; *)
-
-(* A round is steps 2-5 *)
-(* TODO do_round is confusing since it does rounds until evaluation is complete *)
-let rec do_round (g0: aform) (g : aform) (i : int) (rs : int list) =
+let rec do_sumcheck ~g0 ~g ~round ~randoms =
   (* Step 1 *)
-  Printf.printf "#SAT of g%i = %i\n" i (Prover.eval_sharp_sat g);
+  Printf.printf "#SAT of g%i = %i\n" (round - 1) (Prover.eval_sharp_sat g);
 
   (* Step 2 - TODO Explain including this here where in papers it isn't in the round *)
   let g_partial : aform =  Prover.get_partial_sum g in
-  let () = Printf.printf "partial_sum = %s\n" (show_aform g_partial) in
+  let () = Printf.printf "g%i = %s\n" (round - 1) (show_aform g_partial) in
 
   (* Step 3 *)
   (* TODO Print out the details *)
   let result = Verifier.check_partial_sum g g_partial in
-  Printf.printf "g%i == g%i(0) + g%i(1) is %b\n" i (i + 1) (i + 1) result;
+  Printf.printf "g%i == g%i(0) + g%i(1) is %b\n" (round - 1) round round result;
 
   (* Step 4 *)
   let r = Verifier.get_random () in
@@ -57,10 +49,10 @@ let rec do_round (g0: aform) (g : aform) (i : int) (rs : int list) =
   match get_first_free_variable g' with
   | None -> 
     (* Step 7 *)
-    Verifier.oracle_check g0 g (r::rs)
-  | Some _ -> do_round g0 g' (i + 1) (r::rs);;
+    Verifier.oracle_check g0 g (r::randoms)
+  | Some _ -> do_sumcheck ~g0 ~g:g' ~round ~randoms:(r::randoms)
 
 (* Perform repeatedly do the steps of checking / constraining with V's rng *)
-let result = do_round g0 g0 0 [] in
+let result = do_sumcheck ~g0:g0 ~g:g0 ~round:1 ~randoms:[];;
 Printf.printf "Verifier completed step 7 and believes the Prover: %b\n" result
  
